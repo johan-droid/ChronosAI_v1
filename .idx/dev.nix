d7a66450555a474758d0fce5e1289027e98fe026
sahooashutosh2022 <sahooashutosh2022@gmail.com>
2026-03-20 10:59:59 +0000

    fixes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,38 +1,53 @@
+
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  
+  # Using "unstable" to get access to newer packages like nodejs_22
+  channel = "unstable"; # or "stable-23.11"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
     pkgs.python311
     pkgs.python311Packages.pip
     pkgs.python311Packages.virtualenv
   ];
-  
+
   # Sets environment variables in the workspace
   env = {};
-  
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "dbaeumer.vscode-eslint"
-      "esbenp.prettier-vscode"
-      "ms-python.python"
-    ];
-    
-    # Enable previews and customize routing
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # We will add initialization scripts here later if needed
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # We will configure background tasks (like starting the DB connection) here later
-      };
+
+  # VS Code extensions
+  # Use https://open-vsx.org/ to find extensions
+  idx.extensions = [
+    "dbaeumer.vscode-eslint"
+  ];
+
+  # Workspace lifecycle hooks
+  idx.workspace = {
+    # Runs when a workspace is first created
+    onCreate = {
+      # Example: install dependencies with npm
+      # npm-install = "npm install";
+    };
+    # Runs on every workspace startup
+    onStart = {
+      # Example: start a dev server
+      # start-server = "npm run dev";
+    };
+  };
+
+  # Web app preview
+  idx.previews = {
+    enable = true;
+    previews = {
+      # The name of the preview
+      # web = {
+      #   # The command to run to start the server
+      #   command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+      #   # The manager for the preview, "web" is for web apps
+      #   manager = "web";
+      # };
     };
   };
 }
